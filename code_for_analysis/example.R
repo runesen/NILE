5d1b27c6e6ea1879801b5a4649ac54d7106fec0e
Rune <krunechristiansen@math.ku.dk>
2020-05-31 20:16:22 +0200

    small change example

--- code_for_analysis/example.R
+++ code_for_analysis/example.R
@@ -1,6 +1,10 @@
-## The NILE estimator 
+## The NILE estimator
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-source("source.R")
+# source("source.R")
+library(devtools)
+library(splines)
+install_github("runesen/NILE")
+library(NILE)
 
 
 # true (unknown) functional relationship X -> Y (linearly extrapolating beyond "extrap")
@@ -17,7 +21,7 @@ beta0 <- runif(n.splines.true, -1,1)
 alphaA <- alphaEps <- alphaH <- 1/sqrt(3)
 A <- runif(n,-1,1)
 H <- runif(n,-1,1)
-X <- alphaA*A + alphaH*H + alphaEps*runif(n,-1,1) 
+X <- alphaA*A + alphaH*H + alphaEps*runif(n,-1,1)
 Y <- fX(x=X,extrap=c(-.7,.7), beta=beta0) + .3*H + .2*runif(n,-1,1)
 
 x.new <- seq(-2,2,length.out=100)
@@ -38,12 +42,12 @@ fit <- NILE(Y, # response
             f.true = function(x) fX(x,c(-.7,.7), beta0), # if supplied, the true causal function is added to the plot
             par.x = list(lambda=NULL, # positive smoothness penalty for X -> Y, if NULL, it is chosen by CV to minimize out-of-sample AR objective
                          breaks=NULL, # if breaks are supplied, exactly these will be used for splines basis
-                         num.breaks=20, # will result in num.breaks+2 splines, ignored if breaks is supplied. 
+                         num.breaks=20, # will result in num.breaks+2 splines, ignored if breaks is supplied.
                          n.order=4 # order of splines
-                         ), 
-            par.a = list(lambda=NULL, # positive smoothness penalty for fit of residuals onto A. If NULL, we first compute the OLS fit of Y onto X, 
+                         ),
+            par.a = list(lambda=NULL, # positive smoothness penalty for fit of residuals onto A. If NULL, we first compute the OLS fit of Y onto X,
                                       # and then choose lambdaA by CV to minimize the out-of-sample MSE for predicting the OLS residuals
-                         breaks=NULL, # same as above 
-                         num.breaks=4, # same as above 
-                         n.order=4 # same as above 
+                         breaks=NULL, # same as above
+                         num.breaks=4, # same as above
+                         n.order=4 # same as above
                          ))
